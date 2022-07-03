1c45bfab9ca4a6b9a2b131589674b4147ab4dcaf
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-07-03 18:26:36 +0200

    Add unfinished anvil logic

--- src/main/java/com/mischiefsmp/enchanttiers/events/Events.java
+++ src/main/java/com/mischiefsmp/enchanttiers/events/Events.java
@@ -3,6 +3,7 @@ package com.mischiefsmp.enchanttiers.events;
 import com.mischiefsmp.enchanttiers.MischiefEnchantStats;
 import com.mischiefsmp.enchanttiers.TierPlacementResult;
 import com.mischiefsmp.enchanttiers.Utils;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.enchantments.EnchantmentOffer;
@@ -11,6 +12,10 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.enchantment.EnchantItemEvent;
 import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
+import org.bukkit.event.inventory.PrepareAnvilEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class Events implements Listener {
     @EventHandler
@@ -43,5 +48,39 @@ public class Events implements Listener {
             int newLevel = event.getEnchantsToAdd().get(enchantment) * result.tier();
             event.getEnchantsToAdd().put(enchantment, newLevel);
         }
+    }
+
+    @EventHandler
+    public void onPreAnvil(PrepareAnvilEvent event) {
+        ItemStack bookItem = event.getInventory().getStorageContents()[1];
+        if(bookItem == null || bookItem.getType() != Material.ENCHANTED_BOOK)
+            return;
+
+        Location anvilLoc = event.getInventory().getLocation();
+        if(anvilLoc == null)
+            return;
+
+        TierPlacementResult tierResult = Utils.isValidTierPlacement(anvilLoc.getBlock());
+        if(!tierResult.success())
+            return;
+
+        ItemStack resultItem = event.getResult();
+        if(resultItem == null)
+            return;
+
+        EnchantmentStorageMeta bookEnchants = (EnchantmentStorageMeta)bookItem.getItemMeta();
+        ItemMeta resultEnchants = resultItem.getItemMeta();
+        if(bookEnchants == null || resultEnchants == null)
+            return;
+
+        //TODO: Do we remove the enchantment or ignore it if it exists in any capacity?
+        for(Enchantment key : bookEnchants.getStoredEnchants().keySet()) {
+            if(!resultEnchants.hasEnchant(key))
+                resultEnchants.removeEnchant(key);
+
+            resultEnchants.addEnchant(key, key.getStartLevel() * tierResult.tier(), false);
+        }
+
+        resultItem.setItemMeta(resultEnchants);
     }
 }
