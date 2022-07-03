2fc8b2fbb6a5c972b367f806b8391a404ba4844c
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-07-03 23:22:36 +0200

    Add book to book anvil code

--- src/main/java/com/mischiefsmp/enchanttiers/events/Events.java
+++ src/main/java/com/mischiefsmp/enchanttiers/events/Events.java
@@ -11,6 +11,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.enchantment.EnchantItemEvent;
 import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
+import org.bukkit.event.inventory.PrepareAnvilEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 
 public class Events implements Listener {
     @EventHandler
@@ -41,6 +44,37 @@ public class Events implements Listener {
         for(Enchantment enchantment : event.getEnchantsToAdd().keySet()) {
             int newLevel = event.getEnchantsToAdd().get(enchantment) * result.tier();
             event.getEnchantsToAdd().put(enchantment, newLevel);
+        }
+    }
+
+    @EventHandler
+    public void onPreAnvil(PrepareAnvilEvent event) {
+        ItemStack first = event.getInventory().getItem(0);
+        ItemStack second = event.getInventory().getItem(1);
+
+        if(event.getResult() != null && first != null && second != null && first.getType() == Material.ENCHANTED_BOOK && second.getType() == Material.ENCHANTED_BOOK) {
+            EnchantmentStorageMeta firstMeta = (EnchantmentStorageMeta) first.getItemMeta();
+            EnchantmentStorageMeta secondMeta = (EnchantmentStorageMeta) second.getItemMeta();
+            EnchantmentStorageMeta resultMeta = (EnchantmentStorageMeta) event.getResult().getItemMeta();
+            if(firstMeta == null || secondMeta == null || resultMeta == null)
+                return;
+
+            for(Enchantment key : secondMeta.getStoredEnchants().keySet()) {
+                int storedLevel = secondMeta.getStoredEnchants().get(key);
+                if(!firstMeta.hasStoredEnchant(key)) {
+                    //Enchantment doesnt exist in book yet, just add it as is
+                    resultMeta.addStoredEnchant(key, storedLevel, true);
+                } else {
+                    //Enchantment exists, allow upgrading it if the stored level is higher or equal to the one currently applied
+                    int firstLevel = firstMeta.getStoredEnchantLevel(key);
+                    if(storedLevel >= firstLevel) {
+                        resultMeta.removeStoredEnchant(key);
+                        resultMeta.addStoredEnchant(key, firstLevel + 1, true);
+                    }
+                }
+            }
+
+            event.getResult().setItemMeta(resultMeta);
         }
     }
 }
