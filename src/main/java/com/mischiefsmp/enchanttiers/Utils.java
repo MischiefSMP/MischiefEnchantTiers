7147bbfd7a8b64c8adb69e1c876c0cb54b624898
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-07-03 20:50:47 +0200

    Fix issues when tiers list is null

--- src/main/java/com/mischiefsmp/enchanttiers/Utils.java
+++ src/main/java/com/mischiefsmp/enchanttiers/Utils.java
@@ -4,13 +4,19 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 
+import java.util.HashMap;
+
 public class Utils {
     public static TierPlacementResult isValidTierPlacement(Block enchantmentTable) {
         Location loc = enchantmentTable.getLocation().subtract(0, 1, 0);
         String blockId = enchantmentTable.getWorld().getBlockAt(loc).getType().toString();
         String pp = blockId.replaceAll("_", " ").toLowerCase();
 
-        if(!MischiefEnchantStats.getPluginConfig().getTiers().containsKey(blockId)) return new TierPlacementResult(false, false,blockId, pp, 0);
+        HashMap<String, Integer> tiers = MischiefEnchantStats.getPluginConfig().getTiers();
+        if(tiers == null)
+            return new TierPlacementResult(false, false, blockId, pp, 0);
+
+        if(!tiers.containsKey(blockId)) return new TierPlacementResult(false, false,blockId, pp, 0);
 
         for(int z = -1; z < 2; z++) {
             for(int x = -1; x < 2; x++) {
